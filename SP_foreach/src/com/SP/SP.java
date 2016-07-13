651d0acb2b7e7d78216cab715aec7fc76ce6e294
mehulghai1 <mehul.ghai@synechron.com>
2016-07-13 13:08:26 +0530

    ForEach

---
+++ SP_foreach/src/com/SP/SP.java
@@ -0,0 +1,20 @@
+package com.SP;
+
+public class SP {
+
+	public static void main(String[] args) {
+		int[] intary = null;
+		foreachdisplay(intary);
+		
+
+	}
+
+	private static void foreachdisplay(int[] intary) 
+	{
+		for (int i=0 ; i <=20 ; i++) {
+			System.out.println("Print value :-"+i);
+		}
+		
+	}
+
+}
