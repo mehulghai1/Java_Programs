189fae6bce4488168d7def924b9998762a826a42
mehulghai1 <mehul.ghai@synechron.com>
2016-07-13 13:04:24 +0530

    StaticBlock

---
+++ NinethPro_StaticBlock/src/com/SB/Block.java
@@ -0,0 +1,11 @@
+package com.SB;
+
+public class Block {
+static{
+	System.out.println("Static block is invoked");
+}
+	public static void main(String[] args) {
+		System.out.println("Hello to the world of Static Block");
+	}
+
+}
